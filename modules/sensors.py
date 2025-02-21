aec04e29fc4f766011fbfe54b108f4e45c639f4a
Charis <acharisd1200@alumno.ipn.mx>
2025-02-21 14:45:59 -0600

    Added a calibration factor
    As the ADC works with small voltages, we reduce them before the measure;
    with this new function we can get the original value.

--- modules/sensors.py
+++ modules/sensors.py
@@ -1,15 +1,17 @@
 from machine import ADC,Pin
 
 class Sensor:
-    def __init__(self, pin_number):
+    def __init__(self, pin_number, calibration_factor=1.0,
+                 attenuation=ADC.ATTN_0DB, resolution=ADC.WIDTH_12BIT):
         """
         Initialize the ADC sensor.
         
         :param pin_number: The GPIO pin number to which the sensor is connected.
+        :param calibration_factor: The factor to calibrate the reading
         """
-        self.adc = ADC(Pin(pin_number))
-        self.adc.width(ADC.WIDTH_12BIT)
-        self.adc.atten(ADC.ATTN_0DB)
+        self.adc = ADC(pin=Pin(pin_number), atten=attenuation)
+        self.adc.block().init(bits=ADC.WIDTH_12BIT)
+        self.calibration_factor = calibration_factor
 
     def raw_read(self):
         """
@@ -28,3 +30,11 @@ class Sensor:
         #TODO This value will need some tuning and also some configurable parameters
         voltage = int(self.adc.read_uv() / 1000) - 21
         return voltage
+
+    def read(self):
+        """
+        Computes a calibrated voltage
+
+        :return calibrated voltage
+        """
+        return self.read_voltage() * self.calibration_factor

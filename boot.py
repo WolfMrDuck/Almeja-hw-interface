0bbd1e1cb7d7910c2f8cb66032beb49d95292436
Charis <acharisd1200@alumno.ipn.mx>
2025-02-21 19:28:50 -0600

    Sensors working
    Just need to experimentally put some calibration factors to get the
    right measure.

--- boot.py
+++ boot.py
@@ -4,4 +4,27 @@ import json
 config_file = open("config.json",'r')
 cfg = json.load(config_file)
 
-solar_voltmeter = Sensor(pin_number=cfg['pin']['solar'])
+voltmeters = {
+        "solar": Sensor(pin_number=cfg['pin']['solar'],
+                        calibration_factor=cfg['factor']['solar']),
+        "wind": Sensor(pin_number=cfg['pin']['wind'],
+                        calibration_factor=cfg['factor']['wind']),
+        "batt": Sensor(pin_number=cfg['pin']['batt_vm'],
+                        calibration_factor=cfg['factor']['batt_vm']),
+        }
+
+ammeters = {
+        "source": Sensor(pin_number=cfg['pin']['source'],
+                        calibration_factor=cfg['factor']['source']),
+        "batt": Sensor(pin_number=cfg['pin']['batt_amm'],
+                        calibration_factor=cfg['factor']['batt_amm']),
+        }
+
+thermometers = {
+        "therm1": Sensor(pin_number=cfg['pin']['batt_temp1'],
+                        calibration_factor=cfg['factor']['batt_temp1']),
+        "therm2": Sensor(pin_number=cfg['pin']['batt_temp2'],
+                        calibration_factor=cfg['factor']['batt_temp2']),
+        "therm3": Sensor(pin_number=cfg['pin']['batt_temp3'],
+                        calibration_factor=cfg['factor']['batt_temp3']),
+        }
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
 
 while True:
-    print("V: ", solar_voltmeter.read(), "mV")
-    time.sleep(2)
+    for key, voltmeter in voltmeters.items():
+        print(f"{key} voltage value is: {voltmeter.read()} mV")
+    for key, ammeter in ammeters.items():
+        print(f"{key} current value is: {ammeter.read()} mA")
+    for key, thermometer in thermometers.items():
+        print(f"{key} temp value is: {thermometer.read()} m°C")
+    time.sleep(5)

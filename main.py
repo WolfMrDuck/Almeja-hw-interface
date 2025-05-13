bfe684027ec7f6aecf5dbf8df05001a78e60e01f
Charis <acharisd1200@alumno.ipn.mx>
2025-05-13 10:43:45 -0600

    Safe switching added
    To avoid a short circuit if both sources are enable.

--- main.py
+++ main.py
@@ -44,31 +44,43 @@ def thr_send_data():
         print(json_data)
         time.sleep(cfg['core']['read_freq'])
 
+def safe_solar_on():
+    wind.off()
+    solar.on()
+
+def safe_wind_on():
+    solar.off()
+    wind.on()
+
+def instruction_handler():
+    while True:
+        try:
+            cmd, arg = stdin.readline().split()
+            cmd = cmd.upper()
+            arg = int(arg)
+        except:
+            continue
+        if cmd == 'B':
+            if arg == 0:
+                battery.off()
+            elif arg == 1:
+                battery.on()
+        elif cmd == 'S':
+            if arg == 0:
+                solar.off()
+            elif arg == 1:
+                safe_solar_on()
+        elif cmd == 'W':
+            if arg == 0:
+                wind.off()
+            elif arg == 1:
+                safe_wind_on()
+        elif cmd == 'X':
+            controllers['solar'].set_duty_cycle(arg)
+        elif cmd == 'Y':
+            controllers['wind'].set_duty_cycle(arg)
+
 start_new_thread(thr_send_data, ())
 
-while True:
-    try:
-        cmd, arg = stdin.readline().split()
-        cmd = cmd.upper()
-        arg = int(arg)
-    except:
-        continue
-    if cmd == 'B':
-        if arg == 0:
-            battery.off()
-        elif arg == 1:
-            battery.on()
-    elif cmd == 'S':
-        if arg == 0:
-            solar.off()
-        elif arg == 1:
-            solar.on()
-    elif cmd == 'W':
-        if arg == 0:
-            wind.off()
-        elif arg == 1:
-            wind.on()
-    elif cmd == 'X':
-        controllers['solar'].set_duty_cycle(arg)
-    elif cmd == 'Y':
-        controllers['wind'].set_duty_cycle(arg)
+
+instruction_handler()

0e147594ccbb8914f5bb4cb2f757b913ec634c14
Charis <acharisd1200@alumno.ipn.mx>
2025-06-05 14:36:58 -0600

    Added IndicatedSwitch class
    Some indicators were added to the circuit to see if a relay is on or
    off, there were just 3 pins to spare so no every switch would have them.
    
    Fix of the ammeters factor.

--- modules/switch.py
+++ modules/switch.py
@@ -26,3 +26,28 @@ class Switch:
         """
         self.pin.value(0)
         self.state = False
+
+class IndicatedSwitch(Switch):
+    def __init__(self, switch_pin: int, switch_led: int):
+        """
+        Initialize the Switch with an indicator LED.
+    
+        :param int switch_pin: The GPIO pin number to which the switch is connected.
+        :param int switch_led: The GPIO pin number to which the switch's led is connected.
+        """
+        super().__init__(switch_pin)
+        self.led = Pin(switch_led, Pin.OUT)
+
+    def on(self):
+        """
+        Turn the LED on.
+        """
+        super().on()
+        self.led.value(1)
+
+    def off(self):
+        """
+        Turn the LED off.
+        """
+        super().off()
+        self.led.value(0)

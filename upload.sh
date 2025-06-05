16160765a9f7e9df844f581fd1916ed0316866a2
Charis <acharisd1200@alumno.ipn.mx>
2025-06-05 14:55:39 -0600

    More versatile upload script
    The upload script can now work with a command line argument.
    
    Fix bad setting at battery watchdog.

--- upload.sh
+++ upload.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+
+# Check if a command-line argument is provided
+if [ -n "$1" ]; then
+	CONNECT_ARG="connect $1"
+else
+	CONNECT_ARG=""
+fi
+
 echo "=====<<<<<Uploading>>>>>=====";
-uvx mpremote fs cp -r boot.py main.py config.json modules :.;
+uvx mpremote $CONNECT_ARG fs cp -r boot.py main.py config.json modules :.;
 echo "=====<<<<<Done>>>>>=====";
-uvx mpremote fs ls;
-uvx mpremote fs ls modules;
-uvx mpremote reset;
+uvx mpremote $CONNECT_ARG fs ls;
+uvx mpremote $CONNECT_ARG fs ls modules;
+uvx mpremote $CONNECT_ARG reset;
